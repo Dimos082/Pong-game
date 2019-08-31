535adb1e80e0c46d71f91bae8490032b069f8358
Dzmitry Monchares <32190460+Dimos082@users.noreply.github.com>
2019-08-31 22:58:44 +0300

    Logic of difficulty selector changed

--- resources/js/controls.js
+++ resources/js/controls.js
@@ -1,7 +1,3 @@
-function button1() {
-    winScore = 40;
-}
-
 // Recreates the initial position of the ball (for reset button)
 function ballReset() {
     ballX = 50;
@@ -10,13 +6,19 @@ function ballReset() {
     ballSpeedY = 10;
     }
 
-    function changeDifficulty() {
-        let easy = AI_easy();
-        let normal = AI_normal();
-        let hard = AI_hard();
-        }
-
 // Changing The Execution Context Of AI function
+function AI_super_easy() {
+    window.AI = function(){
+        let paddle2XCenter = paddle2X + (paddleWidth / 2);
+    if (paddle2XCenter < ballX - 35) {
+         paddle2X += 1;
+    } else if (paddle2XCenter > ballX + 35) {
+         paddle2X -= 1;
+    }
+    console.log('super easy');
+  }
+}
+
 function AI_easy() {
     window.AI = function(){
         let paddle2XCenter = paddle2X + (paddleWidth / 2);
@@ -24,7 +26,8 @@ function AI_easy() {
          paddle2X += 4;
     } else if (paddle2XCenter > ballX + 35) {
          paddle2X -= 4;
-    }console.log('easy');
+    }
+    console.log('easy');
   }
 }
 
@@ -35,7 +38,8 @@ function AI_normal() {
         paddle2X += 15;
     } else if (paddle2XCenter > ballX + 35) {
         paddle2X -= 15;
-    }console.log('normal');
+    }
+    console.log('normal');
   }
 }
 
@@ -46,7 +50,48 @@ function AI_hard() {
         paddle2X += 30;
     } else if (paddle2XCenter > ballX + 35) {
         paddle2X -= 30;
-    }console.log('hard');
+    }
+    console.log('hard');
   }
 }
 
+function AI_super_hard() {
+    window.AI = function(){
+    let paddle2XCenter = paddle2X + (paddleWidth / 2);
+    if (paddle2XCenter < ballX - 35) {
+        paddle2X += 50;
+    } else if (paddle2XCenter > ballX + 35) {
+        paddle2X -= 50;
+    }
+    console.log('impossibru');
+  }
+}
+
+//Difficulty selector
+function changeDifficulty() {
+    let z = document.getElementById("difficulty").value; 
+switch
+(z) { 
+case
+ "super_easy":
+ AI_super_easy()
+    break;
+case
+ "easy":
+ AI_easy()
+    break;
+case
+ "normal":
+ AI_normal()
+    break;  
+case
+ "hard":
+ AI_hard()
+    break; 
+case
+ "super_hard":
+ AI_super_hard()
+    break; 
+}
+console.log(z);
+  }
